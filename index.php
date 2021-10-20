84b01ec06e8152d6288c599b6a6ab055c3b86f7d
dong geun <creased4@gmail.com>
2021-10-20 15:57:48 +0900

    main

--- index.php
+++ index.php
@@ -1,8 +1,37 @@
 <?php
-    print "<h2>PHp is enjoy!</h2>";
-    print "Hello jiny world!<br>";
-    print "I'm about to learn PHP!";
-?>
-<?php
-    phpinfo();
-?>
+    $a =true;
+    if ($a) {
+        echo "a = ture(참) 입니다.";
+    } else{
+        echo "a + false(거짓) 입니다.";
+    }
+
+    echo "<br>";
+
+    $b = TRUE;
+    if ($b) {
+        echo "b = true(참)입니다.";
+    }else{
+        echo "b = false(거짓) 입니다.";
+    }
+    
+    echo"<br>";
+
+    $c = false;
+    if ($c) {
+        echo "c = true(참)입니다.";
+    }else{
+        echo "c = false(거짓) 입니다.";
+    }
+
+    echo"<br>";
+
+    $d = FALSE;
+    if ($d) {
+        echo "d = true(참)입니다.";
+    } else {
+        echo "d = false(거짓)입니다.";
+    }
+
+    echo "<br>";
+    ?>
