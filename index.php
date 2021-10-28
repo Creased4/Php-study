04e9988058e29bd309e51f65a0f526d0873ceefb
dong geun <creased4@gmail.com>
2021-10-28 10:44:43 +0900

    Using if.

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
 <?php
-    class JinyClass
-    {
-
-        public function test($msg)
-        {
-            return $msg;
+    function _coalescing($var, $default){
+        if (isset($var)){
+            if ($var == NULL) {
+                return $var;
+            } else {
+                return $default;
+            }
+        }else {
+            return $default;
         }
     }
 
-    $jinyClass = new JinyClass();
-    echo "JinyClass 인스턴스<br>";
-    echo $JinyClass->test("JinyClass") . "<br>";
+    $x;
+    if (is_null($x)){ 
+        echo "x = NULL입니다.";
+    } else {
+        echo "x = NULL이 아닙니다.";
+    }
 
-    $obj1 = new JinyClass;
-    $obj2 = new JinyClass;
+    echo "<br>";
 
-    echo"클래스 인스턴스1<br>";
-    echo $obj1->test(1) . "<br>";
-
-    echo"클래스 인스턴스2<br>";
-    echo $obj2->test(2) . "<br>";
-?>
+    $username = _coalescing($x,"nobody");
+    echo "username = $username";
+    ?>
